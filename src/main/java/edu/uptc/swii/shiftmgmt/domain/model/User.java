bde38390e053bc47478cce5fd0eee258178f31bf
danieltorrez29 <daniel.torres04@uptc.edu.co>
2024-05-25 21:06:24 -0500

    clean code

--- src/main/java/edu/uptc/swii/shiftmgmt/domain/model/User.java
+++ src/main/java/edu/uptc/swii/shiftmgmt/domain/model/User.java
@@ -1,16 +1,20 @@
 package edu.uptc.swii.shiftmgmt.domain.model;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "users")
 public class User {
     @Id
-    // @GeneratedValue(strategy = GenerationType.AUTO)
     @Getter
     @Setter
     private Integer user_id;
--- src/main/java/edu/uptc/swii/shiftmgmt/domain/repository/UserRepository.java
+++ src/main/java/edu/uptc/swii/shiftmgmt/domain/repository/UserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import edu.uptc.swii.shiftmgmt.domain.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // @Query("{ 'userId' : ?0 }")
-    // public User findByUserId(String userId);
 
 }
